44acc4d506f7769c297a6a4b6ebe524474fe327a
IliaNovikov <ilia.pp.novikov@gmail.com>
2024-03-28 16:57:02 +0500

    Добавил считывание картинки и комментарии

--- app/src/main/java/com/novikov/faceidentifier/MainActivity.kt
+++ app/src/main/java/com/novikov/faceidentifier/MainActivity.kt
@@ -3,14 +3,18 @@ package com.novikov.faceidentifier
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.ImageFormat
 import android.hardware.camera2.CameraCaptureSession
 import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
 import android.hardware.camera2.CaptureRequest
+import android.hardware.camera2.CaptureResult
+import android.media.ImageReader
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.HandlerThread
 import android.util.Log
 import android.view.Surface
 import android.view.SurfaceView
@@ -28,6 +32,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var cameraManager: CameraManager
     private lateinit var binding: ActivityMainBinding
     private var cameraDevice: CameraDevice? = null
+    private lateinit var imageReader: ImageReader
+    private lateinit var handlerThread: HandlerThread
+    private lateinit var handler: Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,18 +42,24 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
 
+        // Проверка разрешений на камеру и внешнее хранилище
         if(checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
             || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
             requestPermissions(arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
+            Toast.makeText(this, "Необходимо дать все разрешения", Toast.LENGTH_LONG).show()
         }
 
+        // Инициализация сервиса для камеры и считывателя изображения
         cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        imageReader = ImageReader.newInstance(1080, 1920, ImageFormat.JPEG, 1)
 
         try {
+            // Получение id фронтальной камеры
             cameraId = cameraManager.cameraIdList.last()
 
             Log.i("camera facing", cameraManager.getCameraCharacteristics(cameraId).get(CameraCharacteristics.LENS_FACING).toString())
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            //Открытие камеры для работы
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
                 cameraManager.openCamera(cameraId, object : CameraDevice.StateCallback(){
                     override fun onOpened(camera: CameraDevice) {
                         cameraDevice = camera
@@ -64,31 +77,56 @@ class MainActivity : AppCompatActivity() {
                     }
 
                 }, null)
-            }
 
-            Handler().postDelayed({
+                //Задержка необходимая для открытия камеры (пока костыль)
+                Handler().postDelayed({
 
-                binding.mainSurfaceView.holder.setSizeFromLayout()
+                    binding.mainSurfaceView.holder.setSizeFromLayout()
 
-                cameraDevice!!.createCaptureSession(listOf(binding.mainSurfaceView.holder.surface), object : CameraCaptureSession.StateCallback(){
-                    override fun onConfigured(session: CameraCaptureSession) {
-                        Log.i("on configured", "yes")
-                        val builder = cameraDevice!!.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
-                        builder.addTarget(binding.mainSurfaceView.holder.surface)
-                        builder.set(CaptureRequest.CONTROL_EFFECT_MODE, CaptureRequest.CONTROL_AF_MODE_AUTO)
-                        session.setRepeatingRequest(builder.build(), null, null)
+                    Log.i("cameraDevice", cameraDevice.toString())
+                    // Создание сессии захвата изображения
+                    cameraDevice!!.createCaptureSession(listOf(binding.mainSurfaceView.holder.surface, imageReader.surface), object : CameraCaptureSession.StateCallback(){
+                        override fun onConfigured(session: CameraCaptureSession) {
+                            Log.i("on configured", "yes")
+                            //Создание запроса на захват
+                            val builder = cameraDevice!!.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                            builder.addTarget(binding.mainSurfaceView.holder.surface)
+                            builder.addTarget(imageReader.surface)
+                            builder.set(CaptureRequest.CONTROL_EFFECT_MODE, CaptureRequest.CONTROL_AF_MODE_AUTO)
+                            //Отправка повторяющегося запроса
+                            session.setRepeatingRequest(builder.build(), object :
+                                CameraCaptureSession.CaptureCallback() {
+                                //Захват изображения каждую итерацию запроса
+                                override fun onCaptureProgressed(session: CameraCaptureSession, request: CaptureRequest, partialResult: CaptureResult) {
+                                    session.capture(builder.build(), null, null)
+                                }
+                            }, null)
+                        }
+
+                        override fun onConfigureFailed(session: CameraCaptureSession) {
+                            Log.e("capture session error", "error")
+                        }
+
+                    }, null)
+
+                }, 1000)
+
+                //Слушателль доступности картинки из считывателя
+                imageReader.setOnImageAvailableListener(object : ImageReader.OnImageAvailableListener {
+                    //Обработку изображения пихать сюда скорее всего
+                    override fun onImageAvailable(reader: ImageReader?) {
+                        val image = reader!!.acquireLatestImage()
+
+                        val buffer = image.planes[0].buffer
+                        Toast.makeText(baseContext, "captured", Toast.LENGTH_SHORT).show()
+
+                        image.close()
                     }
-
-                    override fun onConfigureFailed(session: CameraCaptureSession) {
-                        Log.e("capture session error", "error")
-                    }
-
                 }, null)
-
-
-
-                Log.i("cameraDevice", cameraDevice.toString())
-            }, 1000)
+            }
+            else{
+                Toast.makeText(baseContext, "Нужны все разрешения", Toast.LENGTH_SHORT).show()
+            }
 
         }
         catch (e: Exception){
@@ -99,12 +137,13 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    //Метод для запроса разрешений
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
         if (requestCode == 1){
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
-
+                return
             }
             else{
                 Toast.makeText(this, "Нужно предоставить все разрешения", Toast.LENGTH_LONG).show()
